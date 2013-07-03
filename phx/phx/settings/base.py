584e23f5640da62d40d4fdfacb125e3591ab2e06
josven <jonas.s.svensson@gmail.com>
2013-07-04 01:20:58 +0200

    Rearrangement of external libs

--- phx/phx/settings/base.py
+++ phx/phx/settings/base.py
@@ -110,6 +110,7 @@ STATIC_URL = '/static/'
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = (
     normpath(join(SITE_ROOT, 'static')),
+    normpath(join(SITE_ROOT, 'libs')),
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
