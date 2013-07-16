1bf84f4162fba4c6759182c9ecc027bfe589734b
josven <jonas.s.svensson@gmail.com>
2013-07-16 21:58:35 +0200

    default user model

--- phx/phx/settings/base.py
+++ phx/phx/settings/base.py
@@ -227,6 +227,9 @@ LOCAL_APPS = (
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+AUTH_USER_MODEL = 'auth.User'
+
 ########## END APP CONFIGURATION
 
 
