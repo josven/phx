9d4c0f7c65a87b8a5f92f1bbf44412f8574bd021
josven <jonas.s.svensson@gmail.com>
2014-07-29 20:03:21 +0200

    Serve static via wsgi

--- phx/wsgi.py
+++ phx/wsgi.py
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+from dj_static import Cling
+from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "phx.settings.production")
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
